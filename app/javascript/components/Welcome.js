a3b8382c3cab77b2ddbe41bcb0a693662a05e0dd
grimhamr <rossiadisj@gmail.com>
2022-01-17 22:23:12 +0200

    create react component for Welcome page

---
+++ app/javascript/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Welcome extends React.Component {
+
+
+
+
+
+
+
+  render () {
+    console.log("Welcome component loaded")
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default Welcome
