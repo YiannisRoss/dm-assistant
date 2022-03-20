89018aedb80133bae4f322e0d62dd1b239d4666c
grimhamr <rossiadisj@gmail.com>
2022-03-20 12:13:28 +0200

    add and test createCharacterWindow()}

--- app/javascript/components/CharacterWindow.js
+++ app/javascript/components/CharacterWindow.js
@@ -1,15 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
 class CharacterWindow extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActivated: false
+        };
+    }
 
     render() {
 
-
         return (
             <React.Fragment>
-
-                <h2>CharacterWindow</h2>
+                <div>
+                    <h2>CharacterWindow</h2>
+                    <h3>{this.state.isActivated && character.name}</h3>
+                </div>
 
 
             </React.Fragment>
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -7,12 +7,20 @@ class Navbar extends React.Component {
 
         const listCharacters = this.props.characters.map((character) =>
 
-            <option key={character.id}> {character.name} </option>
+            <option key={character.id} onClick={function () {
+                createCharacterWindow(character)
+            }
+            }> {character.name} </option>
         )
 
         const listMaps = this.props.maps.map((map) =>
             <option key={map.id}> {map.title} </option>
         )
+
+        function createCharacterWindow(character) {
+
+            console.log(`createCharacterWindow called on character ${character.name}`)
+        }
         return (
             <React.Fragment>
 
