349a75de0b1d9ce82934fe0820ddd0271574eecd
grimhamr <rossiadisj@gmail.com>
2022-03-23 00:48:22 +0200

    add custom layout to Game Tracker

--- app/controllers/game_tracker_controller.rb
+++ app/controllers/game_tracker_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GameTrackerController < ApplicationController
+
+  layout "game_tracker"
   def index
     @characters = Character.all
     # @characters.each do |character|
