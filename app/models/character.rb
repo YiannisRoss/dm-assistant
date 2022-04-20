bc9dd62c9106d038b26ba5fc0022f232d92bbccd
grimhamr <rossiadisj@gmail.com>
2022-04-20 12:51:37 +0300

    add auto-creation of random stats and placeholder description

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,20 @@ class Character < ApplicationRecord
 
   validates :name, presence: true
   has_one_attached :image
+
+  before_save :default_values
+
+  private
+  def default_values
+      self.description = 'No description yet' if self.description.empty?
+      if self.stats.empty?
+        self.stats = "STR: #{3+rand(6) + rand(6) + rand(6)}
+DEX: #{3+rand(6) + rand(6) + rand(6)}
+CON: #{3+rand(6) + rand(6) + rand(6)}
+INT: #{3+rand(6) + rand(6) + rand(6)}
+WIS: #{3+rand(6) + rand(6) + rand(6)}
+CHA: #{3+rand(6) + rand(6) + rand(6)}" 
+      end
+  end
+
 end
