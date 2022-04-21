f44f737e5c96488385b0ee894d04311281cf1038
Gateway <rossiadisj@gmail.com>
2022-04-21 21:38:50 +0300

    allow users to sign in with either username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
   has_many :maps, dependent: :destroy
   has_many :characters, dependent: :destroy
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+    conditions[:email].downcase! if conditions[:email]
+where(conditions.to_h).first
+  end
 end
