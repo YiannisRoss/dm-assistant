29779a0be0d5d5a50b8129ce50b7577212e177ee
Gateway <rossiadisj@gmail.com>
2022-04-22 00:55:10 +0300

    add character serialization to JSON format, export as .export file on application/characters.export

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::CharactersController < Api::V1::BaseController
   def index
     respond_to do |format|
       format.json  { render :json =>  Character.all }
+      format.export { send_data(Character.all.to_json, filename: 'characters.export') }
     end
   end
 
@@ -25,7 +26,19 @@ class Api::V1::CharactersController < Api::V1::BaseController
     render json: character
   end
 
+    
+  def export
+    respond_with :api, :v1, export_user_characters
+end
+
   private
+  def export_characters
+      user = current_user
+      user_character_data = serialize(user.characters, JSON)
+      
+      send_data(user_character_data, filename: 'testfile.boind') 
+      
+  end
 
   def character_params
     params.require(:character).permit(:id, :name, :user_id, :image, :stats, :description)
