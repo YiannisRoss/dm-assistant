7e0ba676dcc81c7ec4f5647eb9301772c2e00f8d
grimhamr <rossiadisj@gmail.com>
2022-05-04 16:12:20 +0300

    retrieve equipment from dndapi, remove bullets from list elements

--- app/javascript/components/InfoPanel.js
+++ app/javascript/components/InfoPanel.js
@@ -4,25 +4,42 @@ import "../../assets/stylesheets/InfoPanel.scss"
 class InfoPanel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            isExpanded: false ,
-            panelInfo: ''}
+        this.state = {
+            isExpanded: false,
+            panelInfo: {
+                results: []
+            }
+        }
     }
 
     async getData() {
-        
+
         console.log(`fetching ${this.props.infoName} data from https://www.dnd5eapi.co/api/${this.props.apiTarget}`)
         let APIData = fetch(`https://www.dnd5eapi.co/api/${this.props.apiTarget}`, {
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             }
-          }).then(response => response.json())
-          .then(data => {
-              this.setState({
-                panelInfo: data
-              }) 
-          })
+        }).then(response => response.json())
+            .then(data => {
+                this.setState({
+                    panelInfo: data
+                })
+            })
 
+    }
+
+    populateListItems() {
+        let listItems
+        if (this.state.panelInfo.results[0]) {
+            const numberOfItemArrays = this.state.panelInfo.results.length
+            for (let i = 0; i < numberOfItemArrays; i++) {
+                listItems = listItems + this.state.panelInfo.results[0].map((item) => <li>{number}</li>);
+            }
+            console.log('this.state.panelInfo.results:')
+            console.log(this.state.panelInfo.results)
+            this.state.panelInfo.results[0].map((item) => <li>{number}</li>);
+        }
+        return listItems
     }
 
     componentDidMount() {
@@ -31,12 +48,16 @@ class InfoPanel extends React.Component {
 
     }
 
+
     render() {
         const { infoName, apiTarget } = this.props;
+        let listItems = this.state.panelInfo.results.map((item) => <li>{item.name}</li>);
+
+
+
         let expandedPanel = <div className="expanded-panel">
 
-             PANEL INFO
-            {/* {this.state.panelInfo} */}
+            {listItems}
 
         </div>
 
@@ -55,9 +76,18 @@ class InfoPanel extends React.Component {
                         isExpanded: false
                     });
                 }}>
-                   <button onClick={() => {
-                       console.log(this.state.panelInfo)
-                   }} />
+                    <button onClick={() => {
+                        console.log(this.state.panelInfo)
+                        console.log('results:')
+                        console.log(this.state.panelInfo.results)
+                        console.log('results length: ')
+                        console.log(this.state.panelInfo.results.length)
+                        console.log('result number 0:')
+                        console.log(this.state.panelInfo.results[0])
+                        console.log('list items :')
+                        console.log(listItems)
+
+                    }} />
                     {infoName}
                     {this.state.isExpanded && expandedPanel}
 
