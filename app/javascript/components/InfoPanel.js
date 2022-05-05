0c482ffe77e0137e5a457ac0ab4d6617d198c130
grimhamr <rossiadisj@gmail.com>
2022-05-05 20:40:20 +0300

    add api call and response parsing on info panel

--- app/javascript/components/InfoPanel.js
+++ app/javascript/components/InfoPanel.js
@@ -6,6 +6,9 @@ class InfoPanel extends React.Component {
         super(props);
         this.state = {
             isExpanded: false,
+            isItemSelected: false,
+            selectedItem: null,
+            selectedItemData: {},
             panelInfo: {
                 results: []
             }
@@ -27,26 +30,68 @@ class InfoPanel extends React.Component {
 
     }
 
+    async getItemData(item) {
+        let APIData = fetch(`https://www.dnd5eapi.co${item.url}`, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => response.json())
+            .then(data => {
+                this.setState({
+                    selectedItemData: data
+                })
+            })
+
+    }
+
+    toggleInfoPanel(item) {
+
+        this.getItemData(item)
+        this.setState({
+            selectedItem: item,
+            isItemSelected: true
+        })
+
+    }
     componentDidMount() {
 
         this.getData()
 
     }
 
-
     render() {
         const { infoName, apiTarget } = this.props;
-        let listItems = this.state.panelInfo.results.map((item) => <li>{item.name}</li>);
-        let expandedPanel = <div className="expanded-panel">
+        const { selectedItemData } = this.state;
+        let listItems = this.state.panelInfo.results.map((item) =>
+            <li onClick={() => {
+                this.toggleInfoPanel(item)
+
+            }}>{item.name}</li>);
+        let expandedList = <div className="expanded-panel">
 
             {listItems}
 
         </div>
 
+        let itemProperties = <ul>
+            <li>{selectedItemData.name}</li>
+            <li>Weight: {selectedItemData.weight}</li>
+            <li>{selectedItemData.cost && (selectedItemData.cost.quantity + selectedItemData.cost.unit)}</li>
+        </ul>
+
+        let itemPanel = <div className="expanded-panel item-panel">
+
+            {selectedItemData != null && (
+                itemProperties)
+            }
+
+        </div>
+
+
         return (
             <React.Fragment>
                 <div className="info-panel" onMouseEnter={() => {
-                    console.log('INFOPANEL mouseenter.')
+                    console.log('INFOPANEL mouse enter.')
                     this.setState({
                         isExpanded: true
                     });
@@ -60,7 +105,7 @@ class InfoPanel extends React.Component {
                 }}>
                     <button onClick={() => {
                         console.log(this.state.panelInfo)
-                        console.log('results:')
+                        console.log('results:  ')
                         console.log(this.state.panelInfo.results)
                         console.log('results length: ')
                         console.log(this.state.panelInfo.results.length)
@@ -68,10 +113,13 @@ class InfoPanel extends React.Component {
                         console.log(this.state.panelInfo.results[0])
                         console.log('list items :')
                         console.log(listItems)
-
+                        console.log('selected item data:')
+                        console.log(this.state.selectedItemData)
+                        console.log('cost:' + selectedItemData.cost.quantity)
                     }} />
                     {infoName}
-                    {this.state.isExpanded && expandedPanel}
+                    {this.state.isItemSelected && itemPanel}
+                    {this.state.isExpanded && expandedList}
 
                 </div>
 
