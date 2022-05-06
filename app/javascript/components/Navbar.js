aebf182a88331cc08ca9c72285b0be39c89abd8b
grimhamr <rossiadisj@gmail.com>
2022-05-06 14:32:23 +0300

    remove unused code

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -9,31 +9,14 @@ import ClassesPanel from "./ClassesPanel";
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
-
-
     }
 
     render() {
         const { createMapWindow, createCharacterWindow, createDefaultCharacter } = this.props;
-        const listCharacters = this.props.characters.map((character) =>
-
-            <option key={character.id} onClick={() => {
-                createCharacterWindow(character)
-            }
-            }> {character.name} </option>
-        )
-
-        const listMaps = this.props.maps.map((map) =>
-            <option key={map.id} onClick={() => {
-                createMapWindow(map)
-            }
-            } > {map.title} </ option>
-        )
 
         return (
             <React.Fragment>
                 <div id='navbar-container'>
-
 
                     <DropdownExpandable
                         dropdownOptions={this.props.characters}
