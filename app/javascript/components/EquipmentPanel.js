831f607af1777674309a59c85f4687155fc2b39f
grimhamr <rossiadisj@gmail.com>
2022-06-05 15:38:32 +0300

    add last searched item to state

--- app/javascript/components/EquipmentPanel.js
+++ app/javascript/components/EquipmentPanel.js
@@ -11,7 +11,8 @@ class EquipmentPanel extends React.Component {
             selectedItemData: {},
             panelInfo: {
                 results: []
-            }
+            },
+            lastSearch: ""
         }
 
         this.itemPanelRef = React.createRef()
@@ -54,6 +55,7 @@ class EquipmentPanel extends React.Component {
     }
 
     searchDropdown() {
+
         let input = document.getElementById("search-input");
 
         if (input) {
@@ -68,7 +70,12 @@ class EquipmentPanel extends React.Component {
                     li[i].style.display = "none";
                 }
             }
+
+            this.setState({
+                lastSearch: input.value
+            })
         }
+
     }
 
     componentDidMount() {
@@ -78,14 +85,14 @@ class EquipmentPanel extends React.Component {
     }
 
     render() {
-        const { selectedItemData } = this.state;
+        const { selectedItemData, lastSearch } = this.state;
         let listItems = this.state.panelInfo.results.map((item, index) =>
             <li key={index} className="item-list-element" onClick={() => {
                 this.showItemInfo(item)
 
             }}>{item.name}</li>);
-        let expandedList = <div className="dropdown-popup expanded-panel">
-            <input type="text" id='search-input' placeholder="Search..." onChange={() => { this.searchDropdown() }}></input>
+        let expandedList = <div className="dropdown-popup expanded-panel" onMouseEnter={() => { this.searchDropdown() }}>
+            <input type="text" id='search-input' placeholder="Search..." value={lastSearch} onChange={() => { this.searchDropdown() }}></input>
             {listItems}
 
         </div >
